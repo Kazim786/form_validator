c839666e5b3ce763ccf3604a9193588e9ba89428
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-21 17:27:28 -0500

    made notes

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ const password2 = document.getElementById('password2')
 //Show input error message
 const showError = (input, message) => {
 
-    const formControl = input.parentElement;
-    formControl.className = "form-control error"
-    const small = formControl.querySelector('small')
-    small.innerText = message
+    const formControl = input.parentElement; //This will go to the direct parent of any given input tag
+    formControl.className = "form-control error" //Manipulate class name for special CSS effects
+    const small = formControl.querySelector('small') //to retrieve small tag that belongs to the specific form control 
+    small.innerText = message //Changes the text to the given message passed into the parameter
     // small.className = ".form-control.error "
     // small.innerHTML = message
 }
