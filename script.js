809d24d128bf030cd2a82acdf4cc50fd40a03524
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-22 00:55:08 -0500

    debugged

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const validLength = (input) => {
 
 const validEmail = (input) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(res.test(input.value.trim())){
+    if(re.test(input.value.trim())){
         showSuccess(input)
     }
     else {
