e6b7d1d1ea5a2c6a1770af5feb063e7ef7b8a96e
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-22 00:56:18 -0500

    used test method

--- script.js
+++ script.js
@@ -33,8 +33,11 @@ const validLength = (input) => {
 
 const validEmail = (input) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(re.test(input.value.trim())){
+    if(re.test(input.value.trim())){ 
         showSuccess(input)
+        // The test() method tests for a match in a string. 
+        // This method returns true if it finds a match, 
+        // otherwise it returns false.
     }
     else {
         showError(input, 'Email is not valid')
